d05b57cd3c8c0a523c04c5136d2b4689bc1385bf
honey <141145990+oneyVerde@users.noreply.github.com>
2025-07-07 02:14:29 +0900

    [Silver V] Title: 다리 놓기, Time: 40 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+import math
+t = int(input())
+for i in range(t):
+    n, m = map(int, input().split())
+
+    print(math.factorial(m) // (math.factorial(n) * math.factorial(m-n)))
