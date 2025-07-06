7550c1b7d9db51929d656ee07876ebf420621bc1
honey <141145990+oneyVerde@users.noreply.github.com>
2025-07-07 01:30:00 +0900

    불필요한 변수 제거

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
@@ -2,33 +2,31 @@ import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
-def dfs(idx, weight):
+def dfs(idx):
     global praise, visit
     visit[idx] = 1
 
     for node in tree[idx]:
         if not visit[node]:
             praise[node] += praise[idx]
-            dfs(node, praise[node])
+            dfs(node)
 
 
 num_staff, num_praise = map(int, input().split())
 superior = list(map(int, input().split()))
 
-tree = [[] for _ in range(num_staff)]
+tree = [[] for _ in range(num_staff)] # 인접 리스트
 praise = [0 for _ in range(num_staff)]
 visit = [0 for _ in range(num_staff)]
-has_init = [0 for _ in range(num_staff)]
 for _ in range(num_praise):
     staff, praise_weight = map(int, input().split())
     praise[staff-1] += praise_weight # 한명이 여러번 칭찬 받는 경우도 존재
-    has_init[staff-1] = 1
 
 for i in range(num_staff):
     if superior[i] != -1:
         tree[superior[i]-1].append(i)
 for i in range(num_staff):
     if not visit[i]:
-        dfs(i, praise[i])
+        dfs(i)
 
 print(*praise)
