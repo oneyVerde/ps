89bed0a3e27a445449b2c93a17dae5533c5849f5
honey <141145990+oneyVerde@users.noreply.github.com>
2025-07-07 01:27:26 +0900

    주석 추가

--- 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
+++ 백준/Gold/14267. 회사 문화 1/회사 문화 1.py
@@ -3,25 +3,25 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**6)
 
 def dfs(idx, weight):
-    global dp, visit
+    global praise, visit
     visit[idx] = 1
 
     for node in tree[idx]:
         if not visit[node]:
-            dp[node] += dp[idx]
-            dfs(node, dp[node])
+            praise[node] += praise[idx]
+            dfs(node, praise[node])
 
 
 num_staff, num_praise = map(int, input().split())
 superior = list(map(int, input().split()))
 
 tree = [[] for _ in range(num_staff)]
-dp = [0 for _ in range(num_staff)]
+praise = [0 for _ in range(num_staff)]
 visit = [0 for _ in range(num_staff)]
 has_init = [0 for _ in range(num_staff)]
 for _ in range(num_praise):
     staff, praise_weight = map(int, input().split())
-    dp[staff-1] += praise_weight
+    praise[staff-1] += praise_weight # 한명이 여러번 칭찬 받는 경우도 존재
     has_init[staff-1] = 1
 
 for i in range(num_staff):
@@ -29,6 +29,6 @@ for i in range(num_staff):
         tree[superior[i]-1].append(i)
 for i in range(num_staff):
     if not visit[i]:
-        dfs(i, dp[i])
+        dfs(i, praise[i])
 
-print(*dp)
+print(*praise)
