813c46cc528ad6e864abc61f65a3a115ffa27a3a
honey <141145990+oneyVerde@users.noreply.github.com>
2025-07-14 00:06:23 +0900

    Update 뒤집기.py

--- 백준/Silver/1439. 뒤집기/뒤집기.py
+++ 백준/Silver/1439. 뒤집기/뒤집기.py
@@ -5,9 +5,7 @@ s = list(map(int, input().strip()))
 count = [0, 0]
 before = -1
 for i in s:
-    if before == -1:
-        count[i] += 1
-    if before != -1 and before != i:
+    if before != i:
         count[i] += 1
     before = i
 print(min(count))
