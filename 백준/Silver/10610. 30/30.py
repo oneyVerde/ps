a7e10b23adfcee53a5d84e51846975b25285e1e3
honey <141145990+oneyVerde@users.noreply.github.com>
2025-07-14 00:03:38 +0900

    [Silver IV] Title: 30, Time: 68 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/10610. 30/30.py
@@ -0,0 +1,13 @@
+num = list(input())
+num.sort(reverse=True)
+total = 0
+
+if '0' not in num:
+    print(-1)
+else:
+    for i in num:
+        total += int(i)
+    if total % 3 != 0:
+        print(-1)
+    else:
+        print(''.join(num))
