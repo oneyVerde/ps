676d42aecd0bfb78402b54e9f567fd21c8b48db1
honey <141145990+oneyVerde@users.noreply.github.com>
2025-07-31 23:54:09 +0900

    [Silver I] Title: 점프, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1890. 점프/점프.py
@@ -0,0 +1,22 @@
+n = int(input())
+board = []
+for _ in range(n):
+    board.append(list(map(int, input().split())))
+
+path = [[0]*n for _ in range(n)]
+path[n-1][n-1] = 1
+board[n-1][n-1] = 1
+N = n*n
+for i in range(2*(n-1), -1, -1):
+    for j in range(min(n-1, i), max(0, i-n+1)-1, -1):
+        row, col = j+board[j][i-j], i-j+board[j][i-j]
+        if 0 <= row < n and 0 <= col < n:
+            path[j][i-j] = path[row][i-j] + path[j][col]
+        else:
+            if 0 <= row < n:
+                path[j][i-j] += path[row][i-j]
+            if 0 <= col < n:
+                path[j][i-j] += path[j][col]
+
+
+print(path[0][0])
