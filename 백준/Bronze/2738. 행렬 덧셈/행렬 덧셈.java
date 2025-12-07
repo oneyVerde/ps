7d6e463b4ab9f491089175226bdc3ba4e477140f
jhoney <141145990+oneyVerde@users.noreply.github.com>
2025-12-07 14:03:16 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 188 ms, Memory: 18072 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,11 +1,15 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class Main {
     public void solution() throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
@@ -27,11 +31,14 @@ public class Main {
 
         for (int i=0; i<n; i++) {
             for (int j=0; j<m; j++) {
-                System.out.printf("%d ", mat[i][j]);
+                bw.write(mat[i][j]+" ");
             }
-            System.out.println();
+            bw.write("\n");
         }
 
+        br.close();
+        bw.flush();
+        bw.close();
     }
 
     public static void main(String[] args) throws Exception {
